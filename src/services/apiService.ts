52c4a5f4bfff71cd71b3639fc76dbbe8c280fd68
donelli <49295044+donelli@users.noreply.github.com>
2022-02-14 22:50:41 -0300

    change axios to fetch

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -4,27 +4,19 @@ import Company from '../models/Company';
 
 class ApiService {
 
-   baseUrl = 'http://challenge-back-end-tractian.herokuapp.com/api/v1/'
-   axios: axios.Axios;
+   baseUrl = 'http://challenge-back-end-tractian.herokuapp.com/api/v1'
    
-   constructor() {
-      this.axios = new axios.Axios({
-         baseURL: this.baseUrl,
-         transformResponse: (res) => {
-            return res ? JSON.parse(res) : res;
-         }
-      });
-   }
-
    getCompanies(): Promise<Company[]> {
       return new Promise((resolve, reject) => {
          
-         this.axios.get('/companies')
-         .then(response => {
+         fetch(`${this.baseUrl}/companies`)
+         .then(async response => {
             
             const companies: Company[] = [];
+
+            const data = await response.json();
             
-            for (const companyObject of response.data.data) {
+            for (const companyObject of data.data) {
                const company = new Company(companyObject.name, companyObject.id);
                company.createdAt = new Date(companyObject.createdAt);
                company.updatedAt = new Date(companyObject.updatedAt);
