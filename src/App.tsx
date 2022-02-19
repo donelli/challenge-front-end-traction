d3a79272aa1f471201d447cf8c15177444631ede
donelli <eduardopellenz@hotmail.com>
2022-02-19 12:34:11 -0300

    show current company on main header

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { UserOutlined } from '@ant-design/icons';
 
-import { Layout } from 'antd';
-import { Route, Routes } from 'react-router-dom';
+import { Layout, Space } from 'antd';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import CompanyPage from './pages/company/CompanyPage';
+import Company from './models/Company';
+import apiService from './services/apiService';
 const { Header, Footer } = Layout;
 
 function App() {
   
+  const location = useLocation()
+  const [currentCompany, setCurrentCompany] = useState<Company | undefined>();
+  
+  useEffect(
+    () => {
+      
+      // TODO probably need to use a better way to do this
+      const params = location.pathname.split('/');
+      
+      if (params.length >= 2) {
+        
+        if (!currentCompany || currentCompany.id != params[1]) {
+          apiService.getCompanyById(params[1]).then((company) => {
+            setCurrentCompany(company);
+          });
+        }
+        
+      } else {
+        setCurrentCompany(undefined);
+      }
+      
+    },
+    [location]
+  );
+  
   return (
     <Layout className="layout">
-    <Header>
+    <Header className='main-header'>
       <div className='menu-title'>
-        Challenge Tractian
+        <Link to='/'>Challenge Tractian</Link>
+      </div>
+      <div>
+        {currentCompany && <Space>
+          <UserOutlined />
+          <span>{currentCompany.name}</span>
+        </Space>}
       </div>
     </Header>
-    {/* <Content style={{ padding: '0 50px' }}> */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    {/* </Content> */}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/:companyId" element={<CompanyPage />} />
+    </Routes>
     <Footer style={{ textAlign: 'center' }}>Eduardo Donelli Pellenz @ 2022</Footer>
   </Layout>
   );
