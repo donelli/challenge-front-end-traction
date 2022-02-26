53fc696d373103fc3c93e12cf3a44e45d0208893
donelli <eduardopellenz@hotmail.com>
2022-02-26 14:57:47 -0300

    move user modal form to its own file

--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -4,73 +4,13 @@ import User from "../../models/User";
 import apiService from "../../services/apiService";
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 import Modal from "antd/lib/modal/Modal";
+import UserModalForm from "./UserModalForm";
 
 const { Search } = Input;
 
 interface UsersPageProps {
    companyId: string;
 }
-
-interface UserModalFormProps {
-   user?: User;
-   visible: boolean;
-   savingData: boolean;
-   onCancel: () => void;
-   onCreateEditSubmit: (oldUser: User | undefined, userName: string) => void;
-   errorMessage: string;
-}
-
-interface UserFormData {
-   name: string;
-}
-
-const UserModalForm: React.FC<UserModalFormProps> = ({ visible, onCancel, user, savingData, errorMessage, onCreateEditSubmit }) => {
-   
-   const [form] = Form.useForm<UserFormData>();
-   
-   useEffect(() => {
-
-      if (user) {
-         form.setFieldsValue({ name: user.name });
-      } else {
-         form.setFieldsValue({ name: '' });
-      }
-      
-   }, [ user, form ])
- 
-   const onOk = () => {
-     form.submit();
-   };
-
-   const onFinish = () => {
-      onCreateEditSubmit(user, form.getFieldValue('name') as string);
-   }
-   
-   return (
-     <Modal
-         cancelButtonProps={{ disabled: savingData }}
-         okButtonProps={{ loading: savingData, }}
-         title={ user ? 'Edit user' : 'New user' }
-         visible={visible}
-         onOk={savingData ? undefined : onOk}
-         okText={savingData ? 'Saving...' : 'Ok'}
-         onCancel={onCancel}
-         closable={!savingData}
-         maskClosable={!savingData}
-      >
-         <Form form={form} layout="vertical" name="userForm" initialValues={{ name: user ? user.name : '' }} onFinish={onFinish}>
-            
-            <Form.Item name="name" label="User Name" rules={[{ required: true, message: 'Please input the user name!' }]}>
-               <Input disabled={savingData} />
-            </Form.Item>
-            
-         </Form>
-         
-         {errorMessage && <Alert type="error" message={errorMessage} closable={true} />}
-         
-     </Modal>
-   );
-};
 
 const UsersPage: React.FC<UsersPageProps> = ({ companyId }) => {
    
