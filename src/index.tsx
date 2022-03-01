a434e9e1e72c41939dabb964634607e3943ccd5c
donelli <49295044+donelli@users.noreply.github.com>
2022-03-01 10:52:01 -0300

    add basename to router

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/challenge-front-end-traction">
       <App />
     </BrowserRouter>,
   document.getElementById('root')
